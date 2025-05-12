596b37ea526c69eb856bb3499f13dedc87cff700
Diamondking3125 <zeyadahmedabdallah@gamil.com>
2025-05-12 14:45:40 +0300

    ...

--- README.md
+++ README.md
@@ -1,13 +1,26 @@
-# Dynamic Web Page Project
+# Project introduction
 
-This project requires you to build a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
+I have made a dynamic web-page that has an active element with multi section and a working comment section.
 
----
+## Table of Contents
+- [HTML file](#html-file)
+- [CSS file](#css-file)
+- [JS file](#js-file)
+- [Note](#note)
 
-## Instructions
+## HTML file
 
-The starter project has some HTML and CSS styling to display a static version of the Dynamic Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+I have edited the html to add 2 sections(total: 5) and a comments form I decided to put it inside a section to have the active style and I chose to link the JS file before the body's closing tag.
 
-To get started, open `js/app.js` and start building out the app's functionality
+## CSS file
 
-For more detailed requirements, see the project instructions in the Udacity Classroom.
+I modifed the CSS file so that the comments have a style matching the page's style and added some styles to the nav-bar.
+
+## JS file
+
+I have called all the elements I needed and stored them into variables with meaningful names, also I bulit the nav-bar dynamicly using appendChild and createElement functions, added a smooth scrolling method, an active state, an area where the user's submitted comments are and stored them into localStorage so that they don't disapear on refresh.
+
+
+### Note
+I wanted to ask a question. What's the difference between the smooth scrolling with JS and the smooth scrolling with CSS? Which is better and more efficent? and Why?
+
--- js/app.js
+++ js/app.js
@@ -1,7 +1,132 @@
-/** TODO: Build the navigation bar using JavaScript **/
+const sections = document.querySelectorAll("section");
+const navBar = document.getElementById("navbar__list");
+const commentForm = document.getElementById("comment-form");
+const user = document.getElementById("name");
+const email = document.getElementById("email");
+const text = document.getElementById("comment");
+const submittedComments = document.getElementById("comments-list");
+const commentVerifier = document.getElementById("no-comments");
+const storedComments = JSON.parse(localStorage.getItem("comments"));
 
-/** TODO: Add smooth scrolling **/
+const navList = [
+    { name: "Section 1", id: "section1" },
+    { name: "Section 2", id: "section2" },
+    { name: "Section 3", id: "section3" },
+    { name: "Section 4", id: "section4" },
+    { name: "Section 5", id: "section5" },
+    { name: "Comments", id: "comments" },
+    { name: "User's Comments", id: "submitted__comments" }
+];
 
-/** TODO: Add an active state **/
+const commentList = [];
 
-/** TODO: Add a comment form **/
+navList.forEach(item => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.textContent = item.name;
+    a.href = `#${item.id}`;
+    a.classList.add("menu__link");
+    li.appendChild(a);
+    navBar.appendChild(li);
+});
+
+function setActiveSection() {
+    window.addEventListener("scroll", () => {
+        let activeSection = null;
+
+        sections.forEach(section => {
+            const rect = section.getBoundingClientRect();
+            if (rect.top >= -100 && rect.top < window.innerHeight / 2) {
+                activeSection = section;
+            }
+
+            sections.forEach(sec => sec.classList.remove("active"));
+
+            if (activeSection) {
+                activeSection.classList.add("active");
+            }
+        });
+    });
+}
+
+document.querySelectorAll("#navbar__list a").forEach(link => {
+    link.addEventListener("click", function (event) {
+        event.preventDefault();
+        const targetID = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetID);
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: "smooth" });
+        }
+        setTimeout(() => {
+            sections.forEach(sec => sec.classList.remove("active"));
+            targetElement.classList.add("active");
+        }, 300);
+    });
+});
+
+function addComment() {
+    const obj = {
+        name: user.value,
+        email: email.value,
+        text: text.value,
+        id: Date.now()
+    };
+    user.value = "";
+    email.value = "";
+    text.value = "";
+    commentVerifier.innerHTML = "";
+
+    commentList.push(obj);
+
+    localStorage.setItem("comments", JSON.stringify(commentList));
+
+    displayComments(commentList);
+}
+
+function displayComments(parameter) {
+    submittedComments.innerHTML = "";
+    parameter.forEach(
+        (re) =>
+        (submittedComments.innerHTML += `
+        <li>
+            <div class="comment">
+                <h3>Name: ${re.name}</h3>
+                <p>Email: ${re.email}</p>
+                <p>Comment: ${re.text}</p>
+            </div>
+        </li>
+    `)
+    );
+}
+
+commentForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (user.value === "") {
+        alert("Please enter your name");
+        return;
+    }
+
+    if (email.value === "" && !email.value.includes("@")) {
+        alert("Please enter a valid email adress (e.g., example@domain.com)");
+        return;
+    }
+
+    if (text.value === "") {
+        alert("Please enter a comment");
+        return;
+    }
+
+    addComment();
+    displayComments(commentList);
+});
+
+if (storedComments) {
+    storedComments.forEach(comment => {
+        commentList.push(comment);
+    });
+    displayComments(commentList);
+} else {
+    commentVerifier.innerHTML = `<h3>No comments yet</h3>`;
+}
+
+setActiveSection();
